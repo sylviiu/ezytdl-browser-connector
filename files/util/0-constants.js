e83e46194b0c842f88b47f1403fbd4cb71cc01ca
Syl <hi@sylviiu.dev>
2023-07-28 11:40:36 -0500

    move constants.js to 0-constants.js

--- files/util/0-constants.js
+++ files/util/0-constants.js
@@ -1,6 +1,9 @@
+if(typeof browser == `undefined`) var browser = chrome;
+
 const vars = {
     manifestData: chrome.runtime.getManifest(),
     socketStates: [`CONNECTING`, `OPEN`, `CLOSING`, `CLOSED`],
+    serviceWorker: typeof importScripts == `function`,
 }
 
 const comms = {
