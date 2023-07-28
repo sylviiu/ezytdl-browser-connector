38a92d4fc9e9d4ddf81da3b74d635c35a6d6b0c8
Syl <hi@sylviiu.dev>
2023-07-28 04:49:26 -0500

    use aes-cbc encryption server side and encrypt that key using pubkey (now messages can be of any length)

--- files/util/encryptMessage.js
+++ files/util/encryptMessage.js
@@ -1,11 +1,31 @@
 const encryptMessage = (data, serverKey) => new Promise(async res => {
-    if(typeof data == `object`) data = JSON.stringify(data);
+    if(typeof data == `object`) {
+        data = JSON.stringify(data);
+    } else {
+        data = `${data}`;
+    }
 
-    data = `${data}`;
+    // create a random AES key
+    const aesKey = crypto.getRandomValues(new Uint8Array(32));
+    const useKey = await crypto.subtle.importKey('raw', aesKey, 'AES-CBC', true, ['encrypt', 'decrypt']);
 
+    // encrypt that key using the server's public key
+    const encryptedKey = await crypto.subtle.encrypt({ name: 'RSA-OAEP' }, serverKey, aesKey);
+
+    // create IV
+    const iv = crypto.getRandomValues(new Uint8Array(16));
+    
+    // encrypt using that key
     const buffer = new TextEncoder().encode(data);
+    const encryptedData = await crypto.subtle.encrypt({ name: 'AES-CBC', length: 256, iv }, useKey, buffer);
 
-    const encrypted = await crypto.subtle.encrypt({ name: 'RSA-OAEP' }, serverKey, buffer);
+    const resObj = {
+        key: Array.from(new Uint8Array(encryptedKey)),
+        data: Array.from(new Uint8Array(encryptedData)),
+        iv: Array.from(new Uint8Array(iv)),
+    };
+    
+    console.log(`encrypted message!`, resObj);
 
-    return res(encrypted);
+    return res(JSON.stringify(resObj));
 });
